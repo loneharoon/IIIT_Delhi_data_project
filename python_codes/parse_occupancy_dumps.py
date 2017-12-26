71a2a8662ca93b4d97a76e47e48dead2168f1026
Haroon Rashid <haroonr@iiitd.ac.in>
2017-12-26 15:43:32 +0000

    done with identifying all unique traps

--- python_codes/parse_occupancy_dumps.py
+++ python_codes/parse_occupancy_dumps.py
@@ -9,17 +9,38 @@ Created on Thu Dec 21 22:10:15 2017
 ##
 #%%
 
-
+ num_lines = sum(1 for line in open(fl)) # get total number of lines or records in a file 11962709
 #%%
 fl = "/Volumes/MacintoshHD2/Users/haroonr/Downloads/data_subset.json"
 count = 0
 lis = []
 with open(fl) as infile:
     for line in infile:
-        print(line)
+        #print(line)
         lis.append(eval(line))
         count = count + 1
-        if count == 10:
+        if count == 20:
           break
 
-#%%
+#%% Find all unique traps type in a file
+# trap type is stored in 'oid' field
+# takes 10 minutes to read complete file
+count = 0
+traps = []
+with open(fl) as infile:
+    for line in infile:
+        #print(line)
+        traps.append(eval(line)['oid'])
+        count = count + 1
+        #if count == 20:
+         # break
+unique_traps = np.unique(traps)   
+print(unique_traps)
+['bsnDot11StationAssociate' 'bsnDot11StationAssociateFail'
+ 'bsnDot11StationDeauthenticate' 'bsnDot11StationDisassociate'
+ 'ciscoLwappDot11ClientAssocDataStatsTrap'
+ 'ciscoLwappDot11ClientCoverageHolePreAlarm'
+ 'ciscoLwappDot11ClientDisassocDataStatsTrap'
+ 'ciscoLwappDot11ClientMovedToRunState' 'ciscoLwappDot11ClientSessionTrap'
+ 'ciscoLwappSiIdrDevice']
+#%% Find all packets of a single day and then store them building wise
